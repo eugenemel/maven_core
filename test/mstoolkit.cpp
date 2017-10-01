2589921d0dda5910d4a6156de50f45decdd29762
Eugene Melamud <eugene@calicolabs.com>
2017-09-30 22:02:08 -0700

    code to test parsing of mass spec files

--- test/mstoolkit.cpp
+++ test/mstoolkit.cpp
@@ -4,16 +4,29 @@
 using namespace MSToolkit;
 
 int main(int argc, char** argv) {
-    if (argc == 1) { cerr << "Specifiy massspec sample"; return -1; }
+	    if (argc == 1) { cerr << "Specifiy massspec sample"; return -1; }
+	
+		for(int i=1; i<argc; i++ )  {
+			char* filename = argv[i];
+			cerr << "Testing " << filename << endl;
+			// mzSample* sample = new mzSample();
+			// sample->loadMsToolsSample(filename);
 
-   char* filename = argv[1];
-   mzSample* sample = new mzSample();
-   sample->loadMsToolsSample(filename);
+			mzSample* sample = new mzSample();
+			sample->loadSample(filename);
 
-   for(Scan* scan: sample->scans) {
-        cerr << scan->mslevel << "\t" << scan->totalIntensity() << endl;
-   }
+			unsigned int totalPeaks=0;
+			long double totalInts=0;
 
+			for(Scan* scan: sample->scans) {
+				totalPeaks += scan->nobs();
+				totalInts += scan->totalIntensity();
+				//cerr << scan->mslevel << "\t" << scan->totalIntensity() << endl;
+			}
+
+			cerr << filename << "\t" << sample->scans.size() << "\tpeaks=" << totalPeaks << "\ttotalInt=" << totalInts << endl;
+			delete sample;
+	}
    return 0;
 }
 
