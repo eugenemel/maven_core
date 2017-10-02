eugenemel/maven_core

commit dedbfd8a3b3a95a78080c3bd6ca1b505f23385b7

support for compressed mzML and mzXML files

libmaven/base64.cpp
@@ -4,98 +4,37 @@
 using namespace std;
 
 namespace base64 { 
-/**
- *  * Base64 encode one byte
- *   */
-char encode(unsigned char u) {
-		if(u < 26)  return 'A'+u;
-		if(u < 52)  return 'a'+(u-26);
-		if(u < 62)  return '0'+(u-52);
-		if(u == 62) return '+';
-		return '/';
-}
 
-/**
- *  * Decode a base64 character
- *   */
+std::string decode_base64(const std::string& in) {
+	std::string out;
 
+	std::vector<int> T(256,-1);
+	for (int i=0; i<64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i; 
 
-char decode(char c){
-		if(c >= 'A' && c <= 'Z') return(c - 'A');
-		if(c >= 'a' && c <= 'z') return(c - 'a' + 26);
-		if(c >= '0' && c <= '9') return(c - '0' + 52);
-		if(c == '+') return 62;
-		return 63;
-}
-
-/**
- *  * Return TRUE if 'c' is a valid base64 character, otherwise FALSE
- *   */
-int is_base64(char c) {
-		if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
-				(c >= '0' && c <= '9') || (c == '+')             ||
-				(c == '/')             || (c == '=')) {
-				return true;
+	int val=0, valb=-8;
+	for (unsigned char c : in) {
+		if (T[c] == -1) break;
+		val = (val<<6) + T[c];
+		valb += 6;
+		if (valb>=0) {
+			out.push_back(char((val>>valb)&0xFF));
+			valb-=8;
 		}
-		return false;
-}
-
-char* decode_base64(const string& src) {
-
-    int k, l= src.length();
-    char *buf= (char*) calloc(sizeof(char), l);
-    char *dest = (char*) calloc(sizeof(char), l);
-    char *p= dest;
-
-    /* Ignore non base64 chars as per the POSIX standard */
-    for(k=0, l=0; src[k]; k++) {
-        if(is_base64(src[k])) {
-            buf[l++]= src[k];
-        }
-    }
-
-    for(k=0; k<l; k+=4) {
-        char c1='A', c2='A', c3='A', c4='A';
-        unsigned char b1=0, b2=0, b3=0, b4=0;
-
-        c1= buf[k];
-        if(k+1<l) c2= buf[k+1];
-        if(k+2<l) c3= buf[k+2];
-        if(k+3<l) c4= buf[k+3];
-
-        b1= decode(c1);
-        b2= decode(c2);
-        b3= decode(c3);
-        b4= decode(c4);
-
-        *p++=((b1<<2)|(b2>>4) );
-
-        if(c3 != '=') *p++=(((b2&0xf)<<4)|(b3>>2) );
-        if(c4 != '=') *p++=(((b3&0x3)<<6)|b4 );
-    }
-
-	free(buf);
-	return dest;
+	}
+	return out;
 }
 
 
 vector<float> decode_base64(const string& src, int float_size, bool neworkorder, bool decompress) {
 
-	char* dest=decode_base64(src);
-   	int size = 1+(src.length() * 3/4 - 4)/float_size;
+	string destStr=decode_base64(src);
 
 	if (decompress) {
 #ifdef ZLIB
-		string decodedStr(dest);
-		string uncompStr(mzUtils::decompress_string(decodedStr));
-
-		free(dest);
-		dest = (char*) calloc(sizeof(char), uncompStr.size());
-        for(unsigned int i=0; i<uncompStr.size();i++) dest[i] = uncompStr[i];
-   		size = 1+(uncompStr.size() * 3/4 - 4)/float_size;
+		destStr =mzUtils::decompress_string(destStr);
 #endif 
 	}
-	
+
 
 
 #if (LITTLE_ENDIAN == 1)
@@ -103,10 +42,13 @@ vector<float> decode_base64(const string& src, int float_size, bool neworkorder,
      neworkorder=!neworkorder;
 #endif
 
+    int size = 1+(destStr.size() * 3/4 - 4)/float_size;
 
     //we will cast everything as a float may be this is not wise, but have not found a need for double
     //precission yet
     vector<float> decodedArray(size);
+	const char* dest = destStr.c_str();
+
     ////cerr << "Net=" << neworkorder << " float_size=" << float_size << endl;
     if ( float_size == 8 ) {
         if ( neworkorder == false ) {
@@ -136,65 +78,8 @@ vector<float> decode_base64(const string& src, int float_size, bool neworkorder,
     //for debuging
     //for(int i=0; i < size; i++ ) { cout << "\tok.." << i << " " << size <<" " << decodedArray[i] << endl; }
 
-    free(dest);
     return decodedArray;
-}
 
-
-
-
-unsigned char *encode_base64(const vector<float>& farray) {
-
-
-		int i;
-		unsigned char *p;
-
-		//copy vector to C array
-		int sizeF = farray.size();
-		float* srcF= (float*) calloc(sizeof(float), sizeF);
-		for(i=0; i<sizeF; i++ ) { srcF[i]=farray[i]; } 
-		
-		int size = sizeF*4;
-		unsigned char *src = (unsigned char*) srcF;
-		unsigned char* out= (unsigned char*) calloc(sizeof(char), size*4/3+4);
-		p= out;
-
-		
-		for(i=0; i<size; i+=3) {
-
-				unsigned char b1=0, b2=0, b3=0, b4=0, b5=0, b6=0, b7=0;
-
-				b1 = src[i];
-
-				if(i+1<size)
-						b2 = src[i+1];
-
-				if(i+2<size)
-						b3 = src[i+2];
-
-				b4= b1>>2;
-				b5= ((b1&0x3)<<4)|(b2>>4);
-				b6= ((b2&0xf)<<2)|(b3>>6);
-				b7= b3&0x3f;
-
-				*p++= encode(b4);
-				*p++= encode(b5);
-
-				if(i+1<size) {
-						*p++= encode(b6);
-				} else {
-						*p++= '=';
-				}
-
-				if(i+2<size) {
-						*p++= encode(b7);
-				} else {
-						*p++= '=';
-				}
-
-		}
-		free(srcF);
-		return out;
 }
 
 }//namespace

libmaven/base64.h
@@ -6,19 +6,13 @@
 #include <string.h>
 #include <stdint.h>
 
-
 #ifndef BASE64_H
 #define BASE64_H
 using namespace std;
 
 namespace base64 { 
-		char decode(char c);
-		char encode(unsigned char u);
-		int is_base64(char c);
 		vector<float> decode_base64(const string& src, int float_size, bool networkorder, bool decompress);
-		char* decode_base64(const string& src);
-
-		unsigned char *encode_base64(const vector<float>& farray);
+		std::string decode_base64(const std::string& in);
 
 		/* swap bytes .. borrowed from xmms  GNU*/
 		inline uint32_t swapbytes(uint32_t x) {
@@ -29,11 +23,9 @@ namespace base64 {
 		}
 
 		inline uint64_t swapbytes64(uint64_t x) {
-				return ((((uint64_t)swapbytes((uint32_t)(x & 0xffffffffU)) << 32) |
-										(uint64_t)swapbytes((uint32_t)(x >> 32))));
+			return ((((uint64_t)swapbytes((uint32_t)(x & 0xffffffffU)) << 32) |
+						(uint64_t)swapbytes((uint32_t)(x >> 32))));
 		}
 
 }
 #endif
-
-

libmaven/mzSample.cpp
@@ -337,8 +337,11 @@ void mzSample::parseMzMLChromatogromList(xml_node chromatogramList) {
                                 int precision = 64;
                                 if(attr.count("32-bit float")) precision=32;
 
+								bool decompress = false;
+								if(attr.count("zlib compression")) decompress=true;
+
                                 string binaryDataStr = binaryDataArray.child("binary").child_value();
-                                vector<float>binaryData = base64::decode_base64(binaryDataStr,precision/8,false,false);
+                                vector<float>binaryData = base64::decode_base64(binaryDataStr,precision/8,false,decompress);
 
                                 if(attr.count("time array")) { timeVector = binaryData; }
                                 if(attr.count("intensity array")) { intsVector = binaryData; }
@@ -378,7 +381,7 @@ void mzSample::parseMzMLSpectrumList(xml_node spectrumList) {
     for (xml_node spectrum  = spectrumList.child("spectrum");
             spectrum; spectrum = spectrum.next_sibling("spectrum")) {
         string spectrumId = spectrum.attribute("id").value();
-        cerr << "Processing: " << spectrumId << endl;
+        //cerr << "Processing: " << spectrumId << endl;
 
         if (spectrum.empty()) continue;
         map<string,string>cvParams = mzML_cvParams(spectrum);
@@ -403,13 +406,22 @@ void mzSample::parseMzMLSpectrumList(xml_node spectrumList) {
         map<string,string>scanAttr = mzML_cvParams(scanNode);
         if(scanAttr.count("scan start time")) {
             string rtStr = scanAttr["scan start time"];
-            rt = string2float(rtStr);
+            rt = string2float(rtStr)/60.0;
         }
 
 
-        map<string,string>isolationWindow = mzML_cvParams(spectrum.first_element_by_path("precursorList/precursor/isolationWindow"));
-        string precursorMzStr = isolationWindow["isolation window target m/z"];
-        float precursorMz = 0; if(string2float(precursorMzStr)>0) precursorMz=string2float(precursorMzStr);
+        map<string,string>selectedIon = mzML_cvParams(spectrum.first_element_by_path("precursorList/precursor/selectedIonList/selectedIon"));
+        string precursorMzStr = selectedIon["selected ion m/z"];
+
+		map<string,string>isolationWindow = mzML_cvParams(spectrum.first_element_by_path("precursorList/precursor/isolationWindow"));
+		if ( precursorMzStr.length() == 0) precursorMzStr = isolationWindow["isolation window target m/z"];
+
+		string precursorIsolationStr = isolationWindow["isolation window lower offset"];
+		float precursorIsolationWindow = 1.0;
+		if(string2float(precursorIsolationStr)>0) precursorIsolationWindow=string2float(precursorIsolationStr);
+
+        float precursorMz = 0; 
+		if(string2float(precursorMzStr)>0) precursorMz=string2float(precursorMzStr);
 
         string productMzStr = spectrum.first_element_by_path("product/isolationWindow/cvParam").attribute("value").value();
         float productMz = 0;   if(string2float(productMzStr)>0)   productMz=string2float(productMzStr);
@@ -426,16 +438,20 @@ void mzSample::parseMzMLSpectrumList(xml_node spectrumList) {
             int precision = 64;
             if(attr.count("32-bit float")) precision=32;
 
+			bool decompress = false;
+            if(attr.count("zlib compression")) decompress=true;
+
             string binaryDataStr = binaryDataArray.child("binary").child_value();
             if (!binaryDataStr.empty()) {
-                vector<float>binaryData = base64::decode_base64(binaryDataStr,precision/8,false,false);
+                vector<float>binaryData = base64::decode_base64(binaryDataStr,precision/8,false,decompress);
                 if(attr.count("m/z array")) { mzVector = binaryData; }
                 if(attr.count("intensity array")) { intsVector = binaryData; }
             }
         }
 
-        cerr << " scan=" << scannum << "\tms="<< mslevel << "\tprecMz" << precursorMz << "\t rt=" << rt << endl;
+        //cerr << " scan=" << scannum << "\tms="<< mslevel << "\tprecMz=" << precursorMz << "\t rt=" << rt << "\tisolWin=" << precursorIsolationWindow << endl;
         Scan* scan = new Scan(this,scannum++,mslevel,rt,precursorMz,scanpolarity);
+		scan->isolationWindow = precursorIsolationWindow;
         scan->productMz=productMz;
         scan->filterLine= spectrumId;
         scan->intensity = intsVector;
@@ -709,12 +725,9 @@ Scan* mzSample::parseMzXMLScan(const xml_node& mzxml_scan_node, int scannum) {
     if ( ! peaks.empty() ) {
         string b64String(peaks.child_value());
         if ( b64String.empty()) return _scan;  //no m/z intensity values
-		bool decompress = false;
 
-        //decompress
-        if(strncasecmp(peaks.attribute("compressionType").value(),"zlib",4) == 0) {
-			decompress=true;
-		}
+		bool decompress = false; //decompress?
+        if(strncasecmp(peaks.attribute("compressionType").value(),"zlib",4) == 0) decompress=true;
 
         if (!filterLine.empty()) _scan->filterLine=filterLine;
         if (!scanType.empty())   _scan->scanType= scanType;
@@ -728,8 +741,6 @@ Scan* mzSample::parseMzXMLScan(const xml_node& mzxml_scan_node, int scannum) {
         int precision=32;
         if (!peaks.attribute("precision").empty()) { precision = peaks.attribute("precision").as_int(); }
 
-       // cerr << "new scan=" << scannum << " msL=" << msLevel << " rt=" << rt << " precMz=" << precursorMz << " polar=" << scanpolarity
-       //    << " prec=" << precision << endl;
 
         vector<float> mzint = base64::decode_base64(b64String,precision/8,networkorder,decompress);
         int size = mzint.size()/2;
@@ -737,14 +748,16 @@ Scan* mzSample::parseMzXMLScan(const xml_node& mzxml_scan_node, int scannum) {
         _scan->mz.resize(size);
         _scan->intensity.resize(size);
 
+       //cerr << "new scan=" << scannum << " msL=" << msLevel << " rt=" << rt << " precMz=" << precursorMz << " polar=" << scanpolarity << " prec=" << precision << " byteorder=" << networkorder << " comprezed=" << decompress << "\t" << _scan->totalIntensity() << endl;
+
        // cerr << "Network:" << networkorder << " precision" << precision <<  " size=" << size << endl;
 
         int j=0; int count=0;
         for(int i=0; i < size; i++ ) {
             float mzValue = mzint[j++];
             float intensityValue = mzint[j++];
-            //cerr << mzValue << " " << intensityValue << endl;
             if (mzValue > 0 && intensityValue > 0 ) {
+				//cerr << mzValue << " " << intensityValue << endl;
                 _scan->mz[i]= mzValue;
                 _scan->intensity[i] = intensityValue;
                 count++;

commit d5b8442aec8a77cc19148a86170d38bf69576221

fixed truncated spectra bug introduced by dedbfd8a  enter the commit message for your changes. Lines starting

libmaven/base64.cpp
@@ -3,31 +3,49 @@
 
 using namespace std;
 
-namespace base64 { 
+namespace base64 {
 
-std::string decode_base64(const std::string& in) {
-	std::string out;
+std::string b64decode(const void* data, const size_t len)
+{
 
-	std::vector<int> T(256,-1);
-	for (int i=0; i<64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i; 
+    unsigned char* p = (unsigned char*)data;
+    int pad = len > 0 && (len % 4 || p[len - 1] == '=');
+    const size_t L = ((len + 3) / 4 - pad) * 4;
+    std::string str(L / 4 * 3 + pad, '\0');
 
-	int val=0, valb=-8;
-	for (unsigned char c : in) {
-		if (T[c] == -1) break;
-		val = (val<<6) + T[c];
-		valb += 6;
-		if (valb>=0) {
-			out.push_back(char((val>>valb)&0xFF));
-			valb-=8;
-		}
-	}
-	return out;
+    const int B64index [256] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 62, 63, 62, 62, 63, 52, 53, 54, 55,
+   56, 57, 58, 59, 60, 61,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,
+    7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,
+    0,  0,  0, 63,  0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+   41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
+
+    for (size_t i = 0, j = 0; i < L; i += 4)
+    {
+        int n = B64index[p[i]] << 18 | B64index[p[i + 1]] << 12 | B64index[p[i + 2]] << 6 | B64index[p[i + 3]];
+        str[j++] = n >> 16;
+        str[j++] = n >> 8 & 0xFF;
+        str[j++] = n & 0xFF;
+    }
+    if (pad)
+    {
+        int n = B64index[p[L]] << 18 | B64index[p[L + 1]] << 12;
+        str[str.size() - 1] = n >> 16;
+
+        if (len > L + 2 && p[L + 2] != '=')
+        {
+            n |= B64index[p[L + 2]] << 6;
+            str.push_back(n >> 8 & 0xFF);
+        }
+    }
+    return str;
 }
-
 
 vector<float> decode_base64(const string& src, int float_size, bool neworkorder, bool decompress) {
 
-	string destStr=decode_base64(src);
+    string destStr=b64decode(src.c_str(), src.size());
+
 
 	if (decompress) {
 #ifdef ZLIB
@@ -42,7 +60,9 @@ vector<float> decode_base64(const string& src, int float_size, bool neworkorder,
      neworkorder=!neworkorder;
 #endif
 
-    int size = 1+(destStr.size() * 3/4 - 4)/float_size;
+    int size = 1+(destStr.size() - 4)/float_size;
+    //cerr << "decode_base64 " << float_size << "\t src=" << src.length() << " decodeLen=" << destStr.length() << endl;
+    //cerr << "size="  << size << endl;
 
     //we will cast everything as a float may be this is not wise, but have not found a need for double
     //precission yet
@@ -72,6 +92,7 @@ vector<float> decode_base64(const string& src, int float_size, bool neworkorder,
                 uint32_t t = swapbytes(u[i]);
                 memcpy(&data,&t,4);
                 decodedArray[i] = data;
+                //cerr << i << "\t" << data << endl;
             }
         }
     } 

libmaven/base64.h
@@ -12,7 +12,7 @@ using namespace std;
 
 namespace base64 { 
 		vector<float> decode_base64(const string& src, int float_size, bool networkorder, bool decompress);
-		std::string decode_base64(const std::string& in);
+           std::string b64decode(const void* data, const size_t len);
 
 		/* swap bytes .. borrowed from xmms  GNU*/
 		inline uint32_t swapbytes(uint32_t x) {