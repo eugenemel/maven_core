d3ea6a1552b91527f9969547fcfd54c542c40394
Phillip Seitzer <phillipseitzer@calicolabs.com>
2019-09-03 12:55:16 -0700

    Added main method for testing.

--- libmaven/ThreadSafeSmoother.cpp
+++ libmaven/ThreadSafeSmoother.cpp
@@ -1,46 +1,63 @@
 #include "ThreadSafeSmoother.h"
 #include <cmath>
+#include <iostream>
 
-namespace mzUtils{
+using namespace std;
 
-    using namespace std;
+vector<float> VectorSmoother::smooth(vector<float> data, vector<float> weights){
+    vector<float> smoothedData = vector<float>(data.size(), 0);
 
-    vector<float> VectorSmoother::smooth(vector<float> data, vector<float> weights){
-        vector<float> smoothedData = vector<float>(data.size(), 0);
+    //TODO
 
-        //TODO
+    return smoothedData;
+}
 
-        return smoothedData;
+vector<float> MovingAverageSmoother::getWeights(unsigned long windowSize){
+    float frac = 1 / static_cast<float>(windowSize);
+    return vector<float>(windowSize, frac);
+}
+
+vector<float> GaussianSmoother::getWeights(unsigned long windowSize) {
+
+    double windowSizeDouble = static_cast<double>(windowSize);
+
+    vector<float> weights = vector<float>(windowSize, 0);
+
+    double deltaSigma = 6 / windowSizeDouble; // bounds determined by +/- 3 sigma
+
+    unsigned int index = 0;
+
+    while (index < windowSize) {
+        double sigma = -3 + deltaSigma * index;
+        weights.at(index) = static_cast<float>(getGaussianWeight(sigma)/windowSizeDouble);
+        index++;
     }
 
-    vector<float> MovingAverageSmoother::getWeights(unsigned long windowSize){
-        float frac = 1 / static_cast<float>(windowSize);
-        return vector<float>(windowSize, frac);
-    }
+    return weights;
 
-    vector<float> GaussianSmoother::getWeights(unsigned long windowSize) {
+}
 
-        double windowSizeDouble = static_cast<double>(windowSize);
+double GaussianSmoother::getGaussianWeight(double sigma) {
+    double expVal = -1 / pow(2 * sigma, 2);
+    double divider = sqrt(2 * M_PI * pow(sigma, 2));
+    return (1 / divider) * exp(expVal);
+}
 
-        vector<float> weights = vector<float>(windowSize, 0);
-
-        double deltaSigma = 6 / windowSizeDouble; // bounds determined by +/- 3 sigma
-
-        unsigned int index = 0;
-
-        while (index < windowSize) {
-            double sigma = -3 + deltaSigma * index;
-            weights.at(index) = static_cast<float>(getGaussianWeight(sigma)/windowSizeDouble);
-            index++;
-        }
-
-        return weights;
-
-    }
-
-    double getGaussianWeight(double sigma) {
-        double expVal = -1 / pow(2 * sigma, 2);
-        double divider = sqrt(2 * M_PI * pow(sigma, 2));
-        return (1 / divider) * exp(expVal);
-    }
+/**
+ * For Testing
+ *
+ * To compile:
+ * cd ~/workspace/maven_core/libmaven
+ * clang-omp++ ThreadSafeSmoother.cpp -o ThreadSafeSmoother -Wall -I/usr/local/Cellar/llvm/8.0.1/include/c++/v1
+ *
+ * Execute:
+ * ./ThreadSafeSmoother
+ *
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[]) {
+    cout << "TODO" << endl;
 }
--- libmaven/ThreadSafeSmoother.h
+++ libmaven/ThreadSafeSmoother.h
@@ -1,45 +1,44 @@
 #pragma once
 #include <vector>
 
-namespace mzUtils {
+/**
+* @brief The VectorSmoother class
+* Thread-safe implementations of 1D smoothing (std::vector is thread-safe)
+*/
+class VectorSmoother {
+public:
+   virtual ~VectorSmoother() = 0;
+   std::vector<float> smooth(std::vector<float> data, unsigned long windowSize){
 
-     /**
-     * @brief The VectorSmoother class
-     * Thread-safe implementations of 1D smoothing (std::vector is thread-safe)
-     */
-    class VectorSmoother {
-    public:
-        virtual ~VectorSmoother() = 0;
-        std::vector<float> smooth(std::vector<float> data, unsigned long windowSize){
+       if (windowSize % 2 != 0){
+           windowSize++; //err on side of longer half-windows
+       }
 
-            if (windowSize % 2 != 0){
-                windowSize++; //err on side of longer half-windows
-            }
+       return smooth(data, getWeights(windowSize));
+   }
+private:
+   virtual std::vector<float> getWeights(unsigned long windowSize) = 0;
+   std::vector<float> smooth(std::vector<float> data, std::vector<float> weights);
+};
 
-            return smooth(data, getWeights(windowSize));
-        }
-    private:
-        virtual std::vector<float> getWeights(unsigned long windowSize) = 0;
-        std::vector<float> smooth(std::vector<float> data, std::vector<float> weights);
-    };
+/**
+* @brief MovingAverageSmoother class (subclass of VectorSmoother)
+* equal weights, all based on window size.
+*/
+class MovingAverageSmoother : VectorSmoother {
+    ~MovingAverageSmoother();
+private:
+   std::vector<float> getWeights(unsigned long windowSize);
+};
 
-    /**
-     * @brief MovingAverageSmoother class (subclass of VectorSmoother)
-     * equal weights, all based on window size.
-     */
-    class MovingAverageSmoother : VectorSmoother {
-    private:
-        std::vector<float> getWeights(unsigned long windowSize);
-    };
-
-    /**
-     * @brief GaussianSmoother (subclass of VectorSmoother)
-     * weights sample Gaussian distribution (approximately),
-     * with furthest-out points pinned to +/- 3 sigma.
-     */
-    class GaussianSmoother : VectorSmoother {
-    private:
-        double getGaussianWeight(double sigma);
-        std::vector<float> getWeights(unsigned long windowSize);
-    };
-}
+/**
+* @brief GaussianSmoother (subclass of VectorSmoother)
+* weights sample Gaussian distribution (approximately),
+* with furthest-out points pinned to +/- 3 sigma.
+*/
+class GaussianSmoother : VectorSmoother {
+    ~GaussianSmoother();
+private:
+   double getGaussianWeight(double sigma);
+   std::vector<float> getWeights(unsigned long windowSize);
+};
