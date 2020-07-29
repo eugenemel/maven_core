63466b060c3b45cf625ee1932e686c060f21eb33
Phillip Seitzer <phillipseitzer@calicolabs.com>
2020-07-29 13:03:26 -0700

    Issue 245: Updated debugging print statements.

--- libmaven/directinfusionprocessor.cpp
+++ libmaven/directinfusionprocessor.cpp
@@ -169,22 +169,25 @@ vector<Ms3SingleSampleMatch*> DirectInfusionProcessor::processSingleMs3Sample(mz
 
     if (debug) cout << "Finished computing consensus MS1 scan." << endl;
 
-    //Issue 245 debugging line
     if (debug) {
-        for (unsigned int i = 0; i < ms1Fragment->consensus->mzs.size(); i++) {
-            if (ms1Fragment->consensus->mzs[i] > 818.7 && ms1Fragment->consensus->mzs[i] < 818.8) {
-                cout << "i=" << i << ": mz=" << to_string(ms1Fragment->consensus->mzs[i]) << ", intensity=" << to_string(ms1Fragment->consensus->intensity_array[i]) << endl;
-            }
-        }
-    }
-
-    if (debug) {
+        cout << "scans in consensus MS1 scan:" << endl;
         for (auto it = ms1Fragment->consensus->scanNumMap.begin(); it != ms1Fragment->consensus->scanNumMap.end(); ++it){
             cout << it->first->sampleName << ": " << endl;
             for (auto x : it->second) {
                 cout << x << " ";
             }
             cout << endl;
+        }
+    }
+
+    //Issue 245 debugging line
+    if (debug) {
+        cout << "# m/zs in consensus ms1 scan: " << ms1Fragment->consensus->mzs.size() << endl;
+        cout << "m/z values between 818.7 and 818.8 in consensus ms1 scan:" << endl;
+        for (unsigned int i = 0; i < ms1Fragment->consensus->mzs.size(); i++) {
+            if (ms1Fragment->consensus->mzs[i] > 818.7 && ms1Fragment->consensus->mzs[i] < 818.8) {
+                cout << "i=" << i << ": mz=" << to_string(ms1Fragment->consensus->mzs[i]) << ", intensity=" << to_string(ms1Fragment->consensus->intensity_array[i]) << endl;
+            }
         }
     }
 
