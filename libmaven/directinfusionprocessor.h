130cdadbde4071e0e64fda60ab0b49c8a0cb0776
Phillip Seitzer <phillipseitzer@calicolabs.com>
2020-12-01 10:12:35 -0800

    Issue 319: rename variable.

--- libmaven/directinfusionprocessor.h
+++ libmaven/directinfusionprocessor.h
@@ -1224,7 +1224,7 @@ struct DIPipelineSampleData {
     map<int, DirectInfusionAnnotation*> isAnnotationsByPrecursorRangeId = {};
 
     //Issue 319: store quant measurements for different adduct forms of identified compounds
-    map<Compound*, map<Adduct*, float>> ms1ScanIntensityMap{};
+    map<Compound*, map<Adduct*, float>> compoundQuantByAdduct{};
 
     //Internal Standards Normalization
 
