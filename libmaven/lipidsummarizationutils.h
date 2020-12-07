fa0d13db2d955150dbf0d6aa10a60e6ef5871f96
Phillip Seitzer <phillipseitzer@calicolabs.com>
2020-12-07 15:28:37 -0800

    Issue 321: Added comments in preparation for expansion.

--- libmaven/lipidsummarizationutils.h
+++ libmaven/lipidsummarizationutils.h
@@ -12,13 +12,32 @@ private:
 public:
     static std::pair<std::string, std::vector<std::string>> getNameComponents(std::string lipidName);
 
+    //Level 3 (Molecular Species Level)
     static std::string getAcylChainLengthSummary(std::string lipidName);
+
+    //Level 2 (Species Level)
     static std::string getAcylChainCompositionSummary(std::string lipidName);
+
+    //Level 1 (Lipid Class)
     static std::string getLipidClassSummary(std::string lipidName);
 
+    //LipidMaps 2020: "Full structure level ---> -1 (avoid giving level number for future levels)
+    //this level doesn't have a proper number because it isn't summarized.
+    static std::string getFullStructureLevelSummaryAttributeKey(){return "FULL_STRUCTURE_SUMMARY";}
 
+    //LipidMaps 2020: "Structure defined level" --> level 5
+    static std::string getStructureDefinedLevelSummaryAttributeKey(){return "STRUCTURE_DEFINED_SUMMARY";}
+
+    //LipidMaps 2020: "sn-Position level" --> level 4
+    static std::string getSnPositionLevelSummaryAttributeKey(){return "SN_POSITION_SUMMARY";}
+
+    //LipidMaps 2020: "Molecular Species Level" ---> level 3
     static std::string getAcylChainLengthSummaryAttributeKey(){return "ACYL_CHAIN_LENGTH_SUMMARY";}
+
+    //LipidMaps 2020: "Species Level" ---> level 2
     static std::string getAcylChainCompositionSummaryAttributeKey() {return "ACYL_CHAIN_COMPOSITION_SUMMARY";}
+
+    //Lipid Class ---> level 1
     static std::string getLipidClassSummaryKey() {return "LIPID_CLASS";}
 
 private:
