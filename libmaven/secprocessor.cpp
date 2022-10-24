5f933868999e10a7d61f9348cf06340b35dc0287
Phillip Seitzer <phillipseitzer@calicolabs.com>
2022-10-24 15:26:34 -0700

    Issue 571: Initialized SECSearchParameters and SECTrace classes.

--- libmaven/secprocessor.cpp
+++ libmaven/secprocessor.cpp
@@ -1,1 +1,95 @@
 #include "secprocessor.h"
+
+string SECSearchParameters::encodeParams() {
+
+    string encodedParams;
+
+    //program level
+    encodedParams = encodedParams + "searchVersion" + "=" + searchVersion + ";";
+
+    //SEC trace filling
+    encodedParams = encodedParams + "traceMissingIntensityFill" + "=" + to_string(traceMissingIntensityFill) + ";";
+    encodedParams = encodedParams + "traceMinFractionNumber" + "=" + to_string(traceMinFractionNumber) + ";";
+    encodedParams = encodedParams + "traceMaxFractionNumber" + "=" + to_string(traceMaxFractionNumber) + ";";
+
+    //Peak Picking
+
+    encodedParams = encodedParams + "traceSmoothingType" + "=";
+    if (traceSmoothingType == EIC::SmootherType::GAUSSIAN) {
+        encodedParams = encodedParams + "GAUSSIAN";
+    } else if (traceSmoothingType == EIC::SmootherType::AVG) {
+        encodedParams = encodedParams + "AVG";
+    } else if (traceSmoothingType == EIC::SmootherType::SAVGOL) {
+        encodedParams = encodedParams + "SAVGOL";
+    } else {
+        encodedParams = encodedParams + "OTHER";
+    }
+    encodedParams = encodedParams + ";";
+
+    encodedParams = encodedParams + "traceWindowSize" + "=" + to_string(traceWindowSize) + ";";
+    encodedParams = encodedParams + "traceMinPeakIntensity" + "=" + to_string(traceMinPeakIntensity) + ";";
+    encodedParams = encodedParams + "traceMinPeakSN" + "=" + to_string(traceMinPeakSN);
+    encodedParams = encodedParams + "traceBaselineDropTopX" + "=" + to_string(traceBaselineDropTopX);
+    encodedParams = encodedParams + "tracePeakBoundsMaxIntensityFraction" + "=" + to_string(tracePeakBoundsMaxIntensityFraction);
+
+    return encodedParams;
+}
+
+shared_ptr<SECSearchParameters> SECSearchParameters::decode(string encodedParams) {
+
+    shared_ptr<SECSearchParameters> secSearchParameters = shared_ptr<SECSearchParameters>(new SECSearchParameters());
+
+    unordered_map<string, string> decodedMap = mzUtils::decodeParameterMap(encodedParams);
+
+    //program level
+    if (decodedMap.find("searchVersion") != decodedMap.end()) {
+        secSearchParameters->searchVersion = decodedMap["searchVersion"];
+    }
+
+    //SEC trace filling
+    if (decodedMap.find("traceMissingIntensityFill") != decodedMap.end()) {
+        secSearchParameters->traceMissingIntensityFill = stof(decodedMap["traceMissingIntensityFill"]);
+    }
+    if (decodedMap.find("traceMinFractionNumber") != decodedMap.end()) {
+        secSearchParameters->traceMinFractionNumber = stoi(decodedMap["traceMinFractionNumber"]);
+    }
+    if (decodedMap.find("traceMaxFractionNumber") != decodedMap.end()) {
+        secSearchParameters->traceMaxFractionNumber = stoi(decodedMap["traceMaxFractionNumber"]);
+    }
+
+    //Peak Picking
+    if (decodedMap.find("traceSmoothingType") != decodedMap.end()) {
+        string traceSmoothingType = decodedMap["traceSmoothingType"];
+        if (traceSmoothingType == "GAUSSIAN") {
+            secSearchParameters->traceSmoothingType = EIC::SmootherType::GAUSSIAN;
+        } else if (traceSmoothingType == "AVG") {
+            secSearchParameters->traceSmoothingType = EIC::SmootherType::AVG;
+        } else if (traceSmoothingType == "SAVGOL") {
+            secSearchParameters->traceSmoothingType = EIC::SmootherType::SAVGOL;
+        }
+    }
+    if (decodedMap.find("traceWindowSize") != decodedMap.end()) {
+        secSearchParameters->traceWindowSize = stoi(decodedMap["traceWindowSize"]);
+    }
+    if (decodedMap.find("traceMinPeakIntensity") != decodedMap.end()) {
+        secSearchParameters->traceMinPeakIntensity = stof(decodedMap["traceMinPeakIntensity"]);
+    }
+    if (decodedMap.find("traceMinPeakSN") != decodedMap.end()) {
+        secSearchParameters->traceMinPeakSN = stof(decodedMap["traceMinPeakSN"]);
+    }
+    if (decodedMap.find("traceBaselineDropTopX") != decodedMap.end()) {
+        secSearchParameters->traceBaselineDropTopX = stoi(decodedMap["traceBaselineDropTopX"]);
+    }
+    if (decodedMap.find("tracePeakBoundsMaxIntensityFraction") != decodedMap.end()) {
+        secSearchParameters->tracePeakBoundsMaxIntensityFraction = stof(decodedMap["tracePeakBoundsMaxIntensityFraction"]);
+    }
+
+    return secSearchParameters;
+}
+
+SECTrace::SECTrace(SECTraceType type,
+                   vector<int> fractionNums,
+                   vector<float> rawIntensities,
+                   shared_ptr<SECSearchParameters> params) {
+    //TODO
+}
--- libmaven/secprocessor.h
+++ libmaven/secprocessor.h
@@ -3,6 +3,78 @@
 #ifndef SECPROCESSOR_H
 #define SECPROCESSOR_H
 
+#include "mzUtils.h"
+#include "mzSample.h"
 
+class SECSearchParameters {
+
+    public:
+
+    /** =======================
+     * PROGRAM
+     * @param searchVersion: version of search protocol used to generate results.
+     * ========================*/
+    string searchVersion = "UNKNOWN";
+
+    /** =======================
+     * SEC TRACE filling
+     * comments:
+     * baseline smoothing is the same as peak smoothing.
+     * ========================*/
+     float traceMissingIntensityFill = 0.0f;
+     int traceMinFractionNumber = 1;
+     int traceMaxFractionNumber = 72;
+
+     /** =======================
+      * PEAK PICKING
+      * comments:
+      * baseline smoothing is the same as peak smoothing.
+      * ========================*/
+     EIC::SmootherType traceSmoothingType = EIC::SmootherType::GAUSSIAN;
+     int traceWindowSize = 5;
+     float traceMinPeakIntensity = 1e6f;
+     float traceMinPeakSN = 0.0f;
+     float traceBaselineDropTopX = 80; //EIC is full width
+     float tracePeakBoundsMaxIntensityFraction = 0.1f;
+
+     string encodeParams();
+     shared_ptr<SECSearchParameters> decode(string encodedParams);
+};
+
+enum SECTraceType {Unset=0, Peptide=1, Protein=2};
+
+/**
+ * @brief The SECTrace class
+ *
+ * Contains raw data as well as processed data (interpolated points, smoothed points, peaks, etc).
+ *
+ * This is all kept together because in practice, every time a SECTrace is made,
+ * data should be smoothed and peaks should be picked (no need to separate these responsibilities).
+ */
+class SECTrace {
+
+    SECTraceType type = SECTraceType::Unset;
+
+    vector<int> fractionNums{}; //Includes missing fractions, e.g. from params
+
+    vector<float> rawIntensities{}; //For missing intensities, use traceMissingIntensityFill from params
+    vector<float> smoothedIntensities{}; //based on parameters
+
+    shared_ptr<SECSearchParameters> params;
+
+    vector<Peak> peaks{};
+
+    /**
+     * @brief SECTrace
+     * @param type
+     * @param fractionNums
+     * @param rawIntensities
+     * @param params
+     */
+    SECTrace(SECTraceType type,
+             vector<int> fractionNums,
+             vector<float> rawIntensities,
+             shared_ptr<SECSearchParameters> params);
+};
 
 #endif // SECPROCESSOR_H
