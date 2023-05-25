696684d75ff71db4dc519b7138f46f3003d1c346
Phillip Seitzer <phillipseitzer@calicolabs.com>
2023-05-25 10:05:08 -0700

    Issue 646: Created singleton IsotopeProcessorOptions class.

--- libmaven/isotopicenvelopeutils.cpp
+++ libmaven/isotopicenvelopeutils.cpp
@@ -1,1 +1,9 @@
 #include "isotopicenvelopeutils.h"
+
+//private constructor: this is a singleton
+IsotopeProcessorOptions::IsotopeProcessorOptions(){}
+
+IsotopeProcessorOptions& IsotopeProcessorOptions::instance() {
+    static IsotopeProcessorOptions options;
+    return options;
+}
--- libmaven/isotopicenvelopeutils.h
+++ libmaven/isotopicenvelopeutils.h
@@ -3,4 +3,26 @@
 #ifndef ISOTOPICENVELOPEUTILS_H
 #define ISOTOPICENVELOPEUTILS_H
 
+#include "mzSample.h"
+
+class IsotopeProcessorOptions {
+
+public:
+    static IsotopeProcessorOptions& instance();
+
+    void setOption(string key, string value);
+
+    void printOptions();
+
+private:
+    //Singleton: constructor is private
+    IsotopeProcessorOptions();
+
+    //Singleton: Disable copy constructor
+    IsotopeProcessorOptions(const IsotopeProcessorOptions&) = delete;
+
+    //Singleton: Disable assignment operator
+    IsotopeProcessorOptions& operator= (const IsotopeProcessorOptions&) = delete;
+};
+
 #endif // ISOTOPICENVELOPEUTILS_H
