aa503c25df8fd6ece1677eb3ddbf2a79743fbde7
Phillip Seitzer <phillipseitzer@gmail.com>
2025-05-22 15:21:06 -0700

    Issue 779: Update print function in ScanIsotopicEnvelope class.

--- libmaven/isotopicenvelopeutils.cpp
+++ libmaven/isotopicenvelopeutils.cpp
@@ -1618,15 +1618,19 @@ void ScanIsotopicEnvelope::print() {
 
     stringstream ss;
     ss << std::fixed << setprecision(4);
-    ss << "mz: " << monoIsotopicMass << ", z=" << charge << " :{";
+    ss << "mz: " << monoIsotopicMass
+       << ", z=" << charge
+       << ", N=" << mz.size()
+       << ": {";
 
     ss << std::fixed << setprecision(0);
     for (unsigned int i = 0; i < intensity.size(); i++) {
         if (i > 0) ss << ", ";
         ss << intensity.at(i);
     }
-    ss << "}; Total=";
+    ss << "}; Envelope Intensity=";
     ss << getTotalIntensity();
+    ss << "\n";
 
     cout << ss.str();
 }
